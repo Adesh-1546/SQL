87cc526f2c7930c08d0325b05e92f39632c23d60
Adesh-1546 <adeshpol1234@gmail.com>
2025-05-26 23:00:47 +0530

    Add files via upload

--- Constraints.sql
+++ Constraints.sql
@@ -1,6 +1,7 @@
 CREATE TABLE employees (
   emp_id INT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE,
   age INT CHECK (age >= 18),
   hire_date DATE DEFAULT CURRENT_DATE,
