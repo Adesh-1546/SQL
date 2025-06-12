bd1705df49fca01c8d4d99552e4e48ff82111c2e
Adesh-1546 <adeshpol1234@gmail.com>
2025-06-12 23:07:44 +0530

    Add files via upload

--- Update.sql
+++ Update.sql
@@ -2,9 +2,7 @@ UPDATE employee
 SET age = 20 
 WHERE emp_id = 2 
 
-UPDATE employee where
-SET age = 25 
-WHERE emp_id = 4
+
 
 select * from employee ;
 SELECT * FROM employee ORDER BY emp_id ASC ;
