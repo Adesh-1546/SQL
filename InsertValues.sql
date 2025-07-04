73321b65f0d15131add89e3cdb62c22040a2c4d5
Adesh-1546 <adeshpol1234@gmail.com>
2025-07-04 23:19:40 +0530

    Add files via upload

--- InsertValues.sql
+++ InsertValues.sql
@@ -3,6 +3,8 @@ INSERT INTO demotable (demo_name,demo_position,demo_place,demo_date,demo_salary)
 		   ('Sarah' , 'HR' ,'Hinjewadi', '2021-05-04' , 86000.02 ),
 		   ('Sarah' , 'HR' ,'Hinjewadi', '2021-05-04' , 86000.02 ),
 		   ('Micheal' , 'HR' ,'Chinchwad', '2024-01-20' , 42000.02 ),
+		   ('Micheal' , 'HR' ,'Chinchwad', '2024-01-20' , 42000.02 ),
+		   ('Micheal' , 'HR' ,'Chinchwad', '2024-01-20' , 42000.02 ),
 
 		   ('Vyse' , 'HR' ,'Pune', '2023-05-31' , 82000.02 ),
            ('adesh' , 'java dev', 'chinchwad','2023-05-31',250000),
--- Update.sql
+++ Update.sql
@@ -7,4 +7,6 @@ WHERE emp_id = 2
 select * from employee ;
 select * from employee ;
 select * from employee ;
+select * from employee ;
+select * from employee ;
 SELECT * FROM employee ORDER BY emp_id ASC ;
